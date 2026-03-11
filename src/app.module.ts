970f3ccc8237275403ee297ccb5ce1cc6fbbe2ed
ibroxmovv-cell <ibroxmovv@gmail.com>
2026-03-11 12:16:29 +0500

    Create app.module.ts

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RestaurantsModule } from './restaurants/restaurants.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { BookingsModule } from './bookings/bookings.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.get('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+        ssl: { rejectUnauthorized: false },
+      }),
+      inject: [ConfigService],
+    }),
+    RestaurantsModule,
+    UsersModule,
+    AuthModule,
+    BookingsModule,
+  ],
+})
+export class AppModule {}
